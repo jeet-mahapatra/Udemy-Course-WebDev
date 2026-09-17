fa5db9435d2d36d17c92cf0819a0d72d22803187
jeet-mahapatra <jeetmahapatra2004@gmail.com>
2026-09-17 20:34:48 +0530

    learn about es6 module

---
+++ JAVASCRIPT/part8_adv/appM.js
@@ -0,0 +1,8 @@
+import multiply from "./moduleJS.js"
+import {add, substract  } from "./moduleJS.js";
+
+const sum = add(2,3)
+console.log(sum);
+
+console.log(multiply(5,6));
+
---
+++ JAVASCRIPT/part8_adv/moduleJS.js
@@ -0,0 +1,13 @@
+ function add(a,b){
+    return (a+b)
+}
+function substract(a,b){
+    return a-b;
+}
+
+
+export default function multiply(a,b){
+    return a*b
+}
+
+export  {add , substract}
